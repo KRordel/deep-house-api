e409cee3b046932c9bbc192a91f24d2a0e53b9bb
Korman <romkakordel@gmail.com>
2024-04-16 20:30:50 +0300

    feat users crud

---
+++ app/Http/Controllers/V1/UserController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Requests\V1\CreateUserRequest;
+use App\Http\Requests\V1\UpdateUserRequest;
+use App\Http\Resources\V1\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class UserController extends ApiController
+{
+    public function index(Request $request): ResourceCollection
+    {
+        $params = $request->all();
+
+        $users = User::query()->paginate($params['limit'] ?? 10);
+        return $this->respondWithSuccess(UserResource::collection($users));
+    }
+
+    public function show(int $id): JsonResource
+    {
+        $user = User::query()->findOrFail($id);
+        return $this->respondWithSuccess(new UserResource($user));
+    }
+
+    public function store(CreateUserRequest $request): JsonResponse
+    {
+        $user = User::create($request->validated());
+        return $this->respondWithSuccessCreate(new UserResource($user));
+    }
+
+    public function update(UpdateUserRequest $request, int $id): JsonResource
+    {
+        $user = User::query()->findOrFail($id);
+        $user->update($request->validated());
+
+        return $this->respondWithSuccess(new UserResource($user));
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $user = User::query()->findOrFail($id);
+        $user->delete();
+
+        return $this->noContent();
+    }
+}
